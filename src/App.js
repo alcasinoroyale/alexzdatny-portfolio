6c0694981003132b744ce8373addd808baf03cfa
alcasinoroyale <alexzdatny@gmail.com>
2019-09-09 12:38:09 -0400

    Install packages for react-router-dom and react-redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  return (
+const App = () => {
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <Router>
+        <Route>
+        <h1>Alex Zdatny Portfolio</h1>
+        </Route>
+        <div className="routes">
+        </div>
+      </Router>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
