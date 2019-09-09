eb0f9791eb0f2aace8c17349609080fb1348289b
alcasinoroyale <alexzdatny@gmail.com>
2019-09-09 13:01:20 -0400

    Add social media icons to profile container

--- src/containers/ProfileContainer.js
+++ src/containers/ProfileContainer.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubSquare, faTwitterSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const ProfileContainer = () => {
   return (
@@ -6,6 +8,9 @@ const ProfileContainer = () => {
       <h1>Alex Zdatny</h1>
       <h3>Full Stack Developer | Content Creator | Music Enthusiast </h3>
         <div className="social-media-icons">
+          <a href="https://github.com/alcasinoroyale" className="github-icon" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="git" icon={faGithubSquare}/></a>
+          <a href="https://twitter.com/azdatny" className="twitter-icon" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="twitter" icon={faTwitterSquare}/></a>
+          <a href="https://www.linkedin.com/in/alex-zdatny" className="linkedin-icon" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="linkedin" icon={faLinkedin}/></a>
         </div>
     </div>
   )
