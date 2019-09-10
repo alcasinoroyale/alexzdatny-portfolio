07e74aa21b20094ea708124eb419a89e2595e7b6
alcasinoroyale <alexzdatny@gmail.com>
2019-09-10 17:59:44 -0400

    Add programming icons

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,16 @@ import React from 'react'
 import coding from '../images/coding.jpg'
 import albums from '../images/albums.png'
 import films from '../images/favoritefilms.png'
+import atom from '../icons/atom.png'
+import css from '../icons/css.png'
+import git from '../icons/github.png'
+import html from '../icons/html.png'
+import js from '../icons/javascript.png'
+import jq from '../icons/jquery.png'
+import rails from '../icons/rails.png'
+import react from '../icons/react.png'
+import redux from '../icons/redux.png'
+import ruby from '../icons/ruby.png'
 
 const About = () => {
   return (
@@ -12,10 +22,35 @@ const About = () => {
         <img src={coding} class="coding" alt="coding" />
         <img src={albums} class="albums" alt="albums" />
         </div>
+      <div className="skills">
+        <h1>Skills</h1>
+          <div className="icons">
+            <img src={atom} alt="atom" />
+              <h3>Atom</h3>
+            <img src={css} alt="css" />
+              <h3>CSS 3</h3>
+            <img src={git} alt="git" />
+              <h3>GitHub</h3>
+            <img src={html} alt="html" />
+              <h3>HTML 5</h3>
+            <img src={js} alt="js" />
+              <h3>JavaScript</h3>
+            <img src={jq} alt="jq" />
+              <h3>JQuery</h3>
+            <img src={rails} alt="rails" />
+              <h3>Rails</h3>
+            <img src={react} alt="react" />
+              <h3>React</h3>
+            <img src={redux} alt="redux" />
+              <h3>Redux</h3>
+            <img src={ruby} alt="ruby" />
+              <h3>Ruby</h3>
+          </div>
+      </div>
       <div className="interests">
         <h1>Interests</h1>
         <h2>Favorite Films</h2>
-        <img src={films} class="films" alt="films" />        
+        <img src={films} class="films" alt="films" />
       </div>
     </div>
   )
