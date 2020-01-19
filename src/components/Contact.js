09c3159d9f5c397c055f20144ec87b459cf71c2d
alcasinoroyale <alexzdatny@gmail.com>
2020-01-19 15:51:43 -0500

    Start building contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,22 @@ const Contact = () => {
   return (
     <div className="contact">
       <h1>Contact Me</h1>
-      <img src={headshot} class="headshot" alt="headshot" />
-      <h3>Email: alexzdatny@gmail.com</h3>
-      <h3>Cell: 732-887-3198</h3>
+      <div className="contact-content">
+      <form className="contact-form">
+        <label>Name</label>
+          <input type="text" name="name" />
+        <label>Email</label>
+          <input type="email" name="email" />
+        <label>Message</label>
+          <textarea name="message" />
+        <input type="submit" value="Submit" />
+      </form>
+        <div className="contact-info">
+          <img src={headshot} class="headshot" alt="headshot" />
+          <h4>Cell: 732-887-3198</h4>
+          <h4>Email: alexzdatny@gmail.com</h4>
+        </div>
+      </div>
     </div>
   )
 }
