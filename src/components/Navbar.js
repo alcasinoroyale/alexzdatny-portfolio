6449111cf79f1fdbd5d57edd3a058c6bc0923709
alcasinoroyale <alexzdatny@gmail.com>
2020-01-30 18:32:00 -0500

    Update more nav icons, fix positioning of links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 import resume from '../images/alex-zdatny-resume.pdf'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes, faHome, faFileAlt, faSuitcase } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes, faHome, faFileAlt, faSuitcase, faBlog, faAddressBook } from '@fortawesome/free-solid-svg-icons';
 import ResponsiveMenu from 'react-responsive-navbar';
 
 const Navbar = () => {
@@ -10,14 +10,14 @@ const Navbar = () => {
       <ResponsiveMenu
         menuOpenButton={<FontAwesomeIcon className="bars" icon={faBars}/>}
         menuCloseButton={<FontAwesomeIcon className="exit" icon={faTimes}/>}
-        changeMenuOn="600px"
+        changeMenuOn="650px"
         menu={
           <div className="navMenu">
-            <NavLink to="/" exact><FontAwesomeIcon className="home" icon={faHome}/> Home</NavLink>
-            <a href={resume} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFileAlt}/> Resume</a>
-            <NavLink to="/portfolio" exact><FontAwesomeIcon icon={faSuitcase}/> Portfolio</NavLink>
-            <a href="https://alexzdatny.com" target="_blank" rel="noopener noreferrer">Blog</a>
-            <NavLink to="/contact" exact>Contact</NavLink>
+            <NavLink to="/" exact><FontAwesomeIcon className="nav-icon" icon={faHome}/> Home</NavLink>
+            <a href={resume} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="nav-icon" icon={faFileAlt}/> Resume</a>
+            <NavLink to="/portfolio" exact><FontAwesomeIcon className="nav-icon" icon={faSuitcase}/> Portfolio</NavLink>
+            <a href="https://alexzdatny.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="nav-icon" icon={faBlog}/> Blog</a>
+            <NavLink to="/contact" exact><FontAwesomeIcon className="nav-icon" icon={faAddressBook}/> Contact</NavLink>
           </div>
         }
       />
