5a87402ea3c59ed11e6303fa6cc114520be392d9
alcasinoroyale <alexzdatny@gmail.com>
2020-02-08 20:28:57 -0500

    Remove Contact Form for Deployment

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,19 +6,7 @@ const Contact = () => {
     <div className="contact">
       <h1>Contact Me</h1>
       <div className="contact-content">
-      <form className="contact-form">
-        <div className="name">
-          <label>First Name<input type="text" name="firstName" /></label>
-          <label>Last Name<input type="text" name="lastName" /></label>
-        </div>
-        <div className="info">
-          <label>Email<input type="email" name="email" /></label>
-          <label>Subject<input type="text" name="subject" /></label>
-        </div>
-        <label>Message</label>
-          <textarea name="message" />
-        <input type="submit" value="Submit" />
-      </form>
+      
         <div className="contact-info">
           <img src={headshot} class="headshot" alt="headshot" />
           <h3>CELL</h3>732-887-3198
