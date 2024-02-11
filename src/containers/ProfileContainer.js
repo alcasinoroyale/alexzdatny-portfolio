8077cc884c2c5e3c0b64e35fc1a5de3c5efb40d8
alcasinoroyale <alexzdatny@gmail.com>
2024-02-11 17:33:05 -0500

    Updated Profile Icons

--- src/containers/ProfileContainer.js
+++ src/containers/ProfileContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithubSquare, faTwitterSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithubSquare, faInstagramSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import '../profile.css'
 
 const ProfileContainer = () => {
@@ -10,7 +10,7 @@ const ProfileContainer = () => {
       <h2>Full Stack Developer • Content Creator • Music Enthusiast</h2>
         <div className="social-media-icons">
           <a href="https://github.com/alcasinoroyale" className="github-icon" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="git" icon={faGithubSquare}/></a>
-          <a href="https://twitter.com/azdatny" className="twitter-icon" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="twitter" icon={faTwitterSquare}/></a>
+          <a href="https://www.instagram.com/alcasinoroyale" className="instagram-icon" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="instagram" icon={faInstagramSquare}/></a>
           <a href="https://www.linkedin.com/in/alex-zdatny" className="linkedin-icon" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="linkedin" icon={faLinkedin}/></a>
         </div>
     </div>
